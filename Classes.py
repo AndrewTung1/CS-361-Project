6e2134626bf0dbdab4eca36a78aac38780b767f3
AndrewTung1 <andrewcptung1@gmail.com>
2025-05-03 18:08:05 -0700

    Update to Include Remove/Edit feature and improve UI

--- Classes.py
+++ Classes.py
@@ -1,3 +1,6 @@
+import os
+
+line = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 class TVShow:
     """A TV Show with three attributes: name, status, and user rating for the show"""
     def __init__(self, name, status, rating=None):
@@ -9,6 +12,18 @@ class TVList:
     """Class for the list of tv shows with its various methods"""
     def __init__(self):
         self._list = []
+
+    def refresh(self):
+        os.system('cls')
+        print("Welcome to MyTVShows\n")
+        print("Keep track of your shows by adding them to your own personalized list with your viewing status and personal rating!\n")
+        print(line)
+        print("To view your full list of shows, enter 'View List'.\n")
+        print("To view your list by status, enter one of the statuses ('Completed', 'Watching', 'Plan to Watch', 'Dropped')\n")
+        print("To view your finished and dropped shows sorted by rank, enter 'Ranking'\n")
+        print("To remove a show, enter 'Remove'. And to delete your entire list, enter 'Clear'\n")
+        print("To clear the terminal and refresh the screen, enter 'Home'\n")
+        print(line)
 
     def get_list(self):
         """Method for returning the current list"""
@@ -25,46 +40,77 @@ class TVList:
                 return True
         return False
 
-    def print_full_list(self):
+    def print_list(self, status: str):
         """Method to print the current list of shows"""
+        if status == "View List":
+            print(line, "\n\n")
+            for show in self._list:
+                print("Show Name:", show.name, " Status:", show.status, " Rating:", show.rating)
+                print("\n")
+            print(line, "\n")
+        else:
+            print(line, "\n")
+            for show in self._list:
+                if show.status == status:
+                    print("Show Name:", show.name, " Status:", show.status, " Rating:", show.rating)
+                    print("\n")
+            print(line, "\n")
+
+    def print_ranked(self):
+        """Method to print the list of finished or dropped shows by rank"""
+        print(line, "\n")
+        ranked_shows = []
         for show in self._list:
-            print("Show Name: ", show.name, "Status: ", show.status, "Rating: ", show.rating)
+            if type(show.rating) is int:
+                ranked_shows.append(show)
+        ranked_list = sorted(ranked_shows, key=lambda x: x.rating, reverse=True)
+        for show in ranked_list:
+            print("Show Name:", show.name, " Status:", show.status, " Rating:", show.rating)
             print("\n")
+        print(line, "\n")
 
     def print_completed(self):
         """Method to print all the completed shows"""
+        print(line, "\n")
         for show in self._list:
             if show.status == "Completed":
-                print("Show Name: ", show.name, "Status: ", show.status, "Rating: ", show.rating)
+                print("Show Name:", show.name, " Status:", show.status, " Rating:", show.rating)
                 print("\n")
+        print(line, "\n")
 
     def print_watching(self):
         """Method to print all the currently watching shows"""
+        print(line, "\n")
         for show in self._list:
             if show.status == "Watching":
-                print("Show Name: ", show.name, "Status: ", show.status, "Rating: ", show.rating)
+                print("Show Name:", show.name, " Status:", show.status, " Rating:", show.rating)
                 print("\n")
+        print(line, "\n")
 
     def print_plan_to_watch(self):
         """Method to print all the plan to watch shows"""
+        print(line, "\n")
         for show in self._list:
             if show.status == "Plan to Watch":
-                print("Show Name: ", show.name, "Status: ", show.status, "Rating: ", show.rating)
+                print("Show Name:", show.name, " Status:", show.status, " Rating:", show.rating)
                 print("\n")
+        print(line, "\n")
 
     def print_dropped(self):
         """Method to print all the dropped shows"""
+        print(line, "\n")
         for show in self._list:
             if show.status == "Dropped":
-                print("Show Name: ", show.name, "Status: ", show.status, "Rating: ", show.rating)
+                print("Show Name:", show.name, " Status:", show.status, " Rating:", show.rating)
                 print("\n")
+        print(line, "\n")
 
     def get_show_name(self):
         """Method to request the name of a show the user would like to add"""
         while True:
             show_name = input("What is the name of the show you would like to add?\n")
             if show_name == "Cancel":
-                print("Action canceled.")
+                print("Action canceled.\n")
                 return
             if self.exist_already(show_name) is True:
                 print("Error: TV Show is already in your list.\n")
@@ -74,30 +120,29 @@ class TVList:
     def get_show_rating(self):
         """Method to request the rating of a show from the user"""
         while True:
-            try:
-                user_rating = int(input("How would you rate this show from 1 to 10?\n"))
-                if user_rating == "Cancel":
-                    print("Action canceled.")
-                    return
-                if 1 <= user_rating <= 10:
-                    return user_rating
-                else:
-                    print("Error: Number must be between 1 and 10.\n")
-            except ValueError:
+            user_rating = input("How would you rate this show from 1 to 10?\n")
+            if user_rating == "Cancel":
+                print("Action canceled.\n")
+                return
+            elif user_rating.isdigit() is False:
                 print("Error: Invalid input. Please enter an integer between 1 and 10.\n")
+            else:
+                if int(user_rating) > 10 or int(user_rating) < 1:
+                    print("Error: Invalid input. Please enter an integer between 1 and 10.\n")
+                else:
+                    return int(user_rating)
 
     def get_show_status(self):
         """Method to request the status of a show"""
         while True:
-            try:
-                status = input("Enter the status of this show ('Completed', 'Watching', 'Plan to Watch', 'Dropped':\n")
-                if status == "Cancel":
-                    print("Action canceled.")
-                    return
-                if status == "Completed" or status == "Watching" or status == "Plan to Watch" or status == "Dropped":
-                    return status
-            except ValueError:
-                print("Invalid Status. Please Enter One of the Following: 'Completed', 'Watching', 'Plan to Watch', 'Dropped'\n")
+            status = input("Enter the status of this show ('Completed', 'Watching', 'Plan to Watch', 'Dropped':\n")
+            if status == "Cancel":
+                print("Action canceled.\n")
+                return
+            if status in ["Completed", "Watching", "Plan to Watch", "Dropped"]:
+                return status
+            else:
+                print("Invalid Status. Please enter one of the following: 'Completed', 'Watching', 'Plan to Watch', 'Dropped'\n")
 
     def get_and_add_show(self):
         """Method that combines the get and add methods"""
@@ -113,15 +158,16 @@ class TVList:
                 return
             new_show = TVShow(show_name, show_status, show_rating)
             self.add_show(new_show)
+            print(show_name, " has been added!\n")
         elif show_status == "Plan to Watch" or show_status == "Watching":
             new_show = TVShow(show_name, show_status)
             self.add_show(new_show)
+            print(show_name, "has been added!\n")
 
-    def get_remove_name(self):
+    def get_edit_remove_name(self):
         while True:
-            show_name = input("What is the name of the show you would like to remove?\n")
+            show_name = input("What is the name of the show you would like to edit or remove?\n")
             if show_name == "Cancel":
-                print("Action canceled.")
                 return
             if self.exist_already(show_name) is False:
                 print("Error: The TV show you entered is not currently in your list. Please enter a different show or enter 'Cancel' to cancel the action.\n")
@@ -130,26 +176,75 @@ class TVList:
 
     def remove_show(self):
         """Method to remove a show from the list"""
-        show_name = self.get_remove_name()
+        show_name = self.get_edit_remove_name()
         if show_name is None:
-            print("Action canceled.")
+            print("Action canceled.\n")
             return
-        user_input = input("Are you sure you would like to remove this show? This action cannot be undone. (Y/N)")
-        if user_input == "Y":
-            for show in self._list:
-                if show.name == show_name:
-                    self._list.remove(show)
-            print(show_name, "has been removed.")
         else:
-            print("Action canceled.")
+            user_input = input("Are you sure you would like to remove this show? This action cannot be undone. (Y/N)\n")
+            if user_input == "Y":
+                for show in self._list:
+                    if show.name == show_name:
+                        self._list.remove(show)
+                print(show_name, "has been removed.\n")
+            else:
+                print("Action canceled.\n")
+
+    def get_edited_status(self):
+        while True:
+            new_status = input("What is the new status of this show?\n")
+            if new_status == "Cancel":
+                return
+            elif new_status not in ["Completed", "Watching", "Plan to Watch", "Dropped"]:
+                print("Invalid Status. Please enter one of the following: 'Completed', 'Watching', 'Plan to Watch', 'Dropped'\n")
+            else:
+                return new_status
+
+    def get_edited_rating(self):
+        """Method to request the rating of a show from the user"""
+        while True:
+            user_rating = input("What is your new rating of this show?\n")
+            if user_rating == "Cancel":
+                return
+            elif user_rating.isdigit() is False:
+                print("Error: Invalid input. Please enter an integer between 1 and 10.\n")
+            else:
+                if int(user_rating) > 10 or int(user_rating) < 1:
+                    print("Error: Invalid input. Please enter an integer between 1 and 10.\n")
+                else:
+                    return int(user_rating)
+
+    def edit_show(self):
+        """Method to remove a show from the list"""
+        show_name = self.get_edit_remove_name()
+        if show_name is None:
+            print("Action canceled.\n")
+            return
+        else:
+            user_input = input("Are you sure you would like to edit this show? This action cannot be undone. (Y/N)\n")
+            if user_input == "Y":
+                for show in self._list:
+                    if show.name == show_name:
+                        new_status = self.get_edited_status()
+                        if new_status is None:
+                            print("Action canceled.\n")
+                            return
+                        if new_status == "Completed" or new_status == "Dropped":
+                            new_rating = self.get_edited_rating()
+                            if new_rating is None:
+                                print("Action canceled.\n")
+                                return
+                print(show_name, "has been edited.\n")
+            else:
+                print("Action canceled.\n")
 
     def clear_list(self):
         """Method to remove all shows from the list"""
-        user_input = input("Are you sure you would like to remove all shows from your list? This action cannot be undone. (Y/N)")
+        user_input = input("Are you sure you would like to remove all shows from your list? This action cannot be undone. (Y/N)\n")
         if user_input == "Y":
             for show in self._list:
                 self._list.remove(show)
-            print("Your list of shows has been cleared.")
+            print("Your list of shows has been cleared.\n")
         else:
-            print("Action canceled.")
+            print("Action canceled.\n")
 
--- MyTVShows.py
+++ MyTVShows.py
@@ -1,39 +1,67 @@
 import Classes as c
+import os
 
-valid_actions = ["Add", "Edit", "View List", "Completed", "Plan to Watch", "Watching", "Dropped", "Ranking", "Remove", "Clear", "Exit"]
+valid_actions = ["Add", "Edit", "View List", "Completed", "Plan to Watch", "Watching", "Dropped", "Ranking", "Remove", "Clear", "Exit", "Home"]
+
+line = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+
+def test(tv_list):
+    tv_list.add_show(c.TVShow("The Blacklist", "Completed", 6))
+    tv_list.add_show(c.TVShow("Black Mirror", "Completed", 8))
+    tv_list.add_show(c.TVShow("Severance", "Watching"))
+    tv_list.add_show(c.TVShow("Game of Thrones", "Dropped", 4))
+    tv_list.add_show(c.TVShow("Santa Clarita Diet", "Dropped", 5))
+    tv_list.add_show(c.TVShow("Arcane", "Watching"))
+    tv_list.add_show(c.TVShow("The Wire", "Watching"))
+    tv_list.add_show(c.TVShow("Friends", "Plan to Watch"))
+    tv_list.add_show(c.TVShow("The Sopranos", "Plan to Watch"))
 
 def request_action():
-    user_action = input("What would you like to do?\n")
-    try:
+    while True:
+        user_action = input("What would you like to do?\n")
         if user_action in valid_actions:
-            return user_action
-    except ValueError:
-        print("Invalid Action. Please Enter A Valid Action:\n")
-        print("To view your full list of shows, enter 'View List'.\n")
-        print("To view your list by status, enter one of the statuses ('Completed', 'Watching', 'Plan to Watch', 'Dropped')\n")
-        print("To view your finished and dropped shows sorted by rank, enter 'Ranking'\n")
-        print("To remove a show, enter 'Remove'. And to delete your entire list, enter 'Clear'\n")
+                return user_action
+        else:
+            print(line)
+            print("Invalid Action. Please Enter A Valid Action:\n")
+            print("To view your full list of shows, enter 'View List'.\n")
+            print("To view your list by status, enter one of the statuses ('Completed', 'Watching', 'Plan to Watch', 'Dropped')\n")
+            print("To view your finished and dropped shows sorted by rank, enter 'Ranking'\n")
+            print("To remove a show, enter 'Remove'. And to delete your entire list, enter 'Clear'\n")
+            print("To clear the terminal and refresh the screen, enter 'Home'")
+            print(line,"\n")
 
 def action(tv_list: c.TVList):
     user_action = request_action()
+    if user_action == "Home":
+        tv_list.refresh()
     if user_action == "Add":
         tv_list.get_and_add_show()
-    if user_action == "View List":
-        tv_list.print_full_list()
-    if user_action == "Completed":
-        tv_list.print_completed()
-    if user_action == "Watching":
-        tv_list.print_watching()
-    if user_action == "Plan to Watch":
-        tv_list.print_plan_to_watch()
+    if user_action in ["View List", "Completed", "Watching", "Plan to Watch", "Dropped"]:
+        tv_list.print_list(user_action)
+    if user_action == "Ranking":
+        tv_list.print_ranked()
     if user_action == "Remove":
         tv_list.remove_show()
+    if user_action == "Edit":
+        tv_list.edit_show()
     if user_action == "Clear":
         tv_list.clear_list()
     if user_action == "Exit":
         exit()
 
-print("Welcome to MyTVShows\n")
+
+os.system('cls')
+print("Welcome to MyTVShows!\n")
+print("Keep track of your shows by adding them to your own personalized list with your viewing status and personal rating!\n")
+print(line)
+print("To view your full list of shows, enter 'View List'.\n")
+print("To view your list by status, enter one of the statuses ('Completed', 'Watching', 'Plan to Watch', 'Dropped')\n")
+print("To view your finished and dropped shows sorted by rank, enter 'Ranking'\n")
+print("To remove a show, enter 'Remove'. And to delete your entire list, enter 'Clear'\n")
+print("To clear the terminal and refresh the screen, enter 'Home'")
+print(line, "\n")
 MyTVShows = c.TVList()
+test(MyTVShows)
 while True:
     action(MyTVShows)
